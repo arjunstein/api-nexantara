3c17ef116fcb39cebf9fd7a3a1bc1dc0d29e94b0
Arjunstein <arjun.gnw09@gmail.com>
2025-08-05 09:54:39 +0700

    ♻️ refactor regency endpoint to fetch multiple regencies by province ID

--- src/repository/regency.rs
+++ src/repository/regency.rs
@@ -3,7 +3,7 @@ use sqlx::PgPool;
 use uuid::Uuid;
 use anyhow::Result;
 
-pub async fn get_regency_by_province_id(pool: &PgPool, province_id: Uuid) -> Result<Vec<Regency>> {
+pub async fn get_regencies_by_province_id(pool: &PgPool, province_id: Uuid) -> Result<Vec<Regency>> {
     let regencies = sqlx::query_as::<_, Regency>("SELECT * FROM regencies WHERE province_id = $1 ORDER BY name ASC")
     .bind(province_id)
     .fetch_all(pool)
--- src/services/regency.rs
+++ src/services/regency.rs
@@ -1,9 +1,9 @@
 use crate::models::regency::Regency;
-use crate::repository::regency::{get_regency_by_province_id};
+use crate::repository::regency::{get_regencies_by_province_id};
 use sqlx::PgPool;
 use uuid::Uuid;
 use anyhow::Result;
 
-pub async fn fetch_regency_by_province_id(pool: &PgPool, province_id: Uuid) -> Result<Vec<Regency>> {
-    get_regency_by_province_id(pool, province_id).await
+pub async fn fetch_regencies_by_province_id(pool: &PgPool, province_id: Uuid) -> Result<Vec<Regency>> {
+    get_regencies_by_province_id(pool, province_id).await
 }
